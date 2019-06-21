a8dcaed4f57c64264765f30045c61e0e52dd672f
Jessica Joseph <jessjeanjo@gmail.com>
2019-06-21 13:19:32 -0400

    create a post request straight into an info data struct

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,10 +11,25 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    // Adding a test route
+    router.get("cheese", String.parameter) {req -> String in
+        
+        let name = try req.parameters.next(String.self)
+        
+        return "Cheesy fun time with \(name)"
+    }
 
+    router.post(InfoData.self, at: "info") { req, data -> String in
+        return "Hello \(data.name)"
+    }
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+}
+
+struct InfoData:Content {
+    let name: String
 }
