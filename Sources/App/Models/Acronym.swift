80666c35df247ab1417b59ef61fea25a22171091
Jessica Joseph <jessjeanjo@gmail.com>
2019-06-22 19:19:01 -0400

    fixed target bug and updated acronyms model file

---
+++ Sources/App/Models/Acronym.swift
@@ -0,0 +1,20 @@
+import Vapor
+import FluentSQLite
+
+final class Acronym: Codable {
+    var id: Int?
+    var short: String
+    var long: String
+    
+    init(short: String, long: String) {
+        self.short = short
+        self.long = long
+    }
+}
+
+extension Acronym: Model {
+    typealias Database = SQLiteDatabase
+    typealias ID = Int
+    
+    public static var idKey: IDKey = \Acronym.id
+}
