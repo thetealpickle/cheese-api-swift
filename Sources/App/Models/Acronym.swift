f4ac46c5ccf49bf33dfe3248c75bebb9db2d0096
Jessica Joseph <jessjeanjo@gmail.com>
2019-06-24 11:08:40 -0400

    mysql database with user id

--- Sources/App/Models/Acronym.swift
+++ Sources/App/Models/Acronym.swift
@@ -5,10 +5,12 @@ final class Acronym: Codable {
     var id: Int?
     var short: String
     var long: String
+    var userID: User.ID
     
-    init(short: String, long: String) {
+    init(short: String, long: String, userID: User.ID) {
         self.short = short
         self.long = long
+        self.userID = userID
     }
 }
 
