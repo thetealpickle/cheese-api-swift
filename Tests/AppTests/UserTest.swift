d630a1a3112527052d201d10f14530af9a10d55b
Jessica Joseph <jessjeanjo@gmail.com>
2019-06-26 19:11:01 -0400

    updated test to reset the testing database prior to run

--- Tests/AppTests/UserTest.swift
+++ Tests/AppTests/UserTest.swift
@@ -4,28 +4,60 @@ import XCTest
 import FluentMySQL
 
 final class UserTests: XCTestCase {
+    
+    // MARK: Test User Retrieval
     func testUsersCanBeRetrievedFromAPI() throws {
+        
+        // Reset the database for current test run
+        let revertEnvArgs = ["vapor", "revert", "--all", "-y"]
+        var revertConfig = Config.default()
+        var revertServices = Services.default()
+        var revertEnv = Environment.testing
+        
+        revertEnv.arguments = revertEnvArgs
+        
+        try App.configure(&revertConfig, &revertEnv, &revertServices)
+        let revertApp = try Application(
+            config: revertConfig,
+            environment: revertEnv,
+            services: revertServices
+        )
+        
+        try App.boot(revertApp)
+        try revertApp.asyncRun().wait()
+        
+        // Reset the database for current test run
+        let migrateEnvArgs = ["vapor", "migrate", "-y"]
+        var migrateConfig = Config.default()
+        var migrateServices = Services.default()
+        var migrateEnv = Environment.testing
+        
+        migrateEnv.arguments = migrateEnvArgs
+        
+        try App.configure(&migrateConfig, &migrateEnv, &migrateServices)
+        let migrateApp = try Application(
+            config: migrateConfig,
+            environment: migrateEnv,
+            services: migrateServices
+        )
+        
+        try App.boot(migrateApp)
+        try migrateApp.asyncRun().wait()
+        
+        
+        let conn = try migrateApp.newConnection(to: .mysql).wait()
+        
         let expectedName = "Cucumber"
         let expectedUsername = "cucumberPickle"
-        
-        var config = Config.default()
-        var services = Services.default()
-        var env = Environment.testing
-        
-        try App.configure(&config, &env, &services)
-        let app = try Application(config: config, environment: env, services: services)
-        try App.boot(app)
-        
-        let conn = try app.newConnection(to: .mysql).wait()
         
         let user = User(name: expectedName, username: expectedUsername)
         let savedUser = try user.save(on: conn).wait()
         _ = try User(name: "Bob", username: "bobbiePizza").save(on: conn).wait()
         
-        let responder = try app.make(Responder.self)
+        let responder = try migrateApp.make(Responder.self)
         
         let request = HTTPRequest(method: .GET, url: URL(string: "/api/users")!)
-        let wrappedRequest = Request(http: request, using: app)
+        let wrappedRequest = Request(http: request, using: migrateApp)
         
         let response = try responder.respond(to: wrappedRequest).wait()
         
@@ -33,9 +65,9 @@ final class UserTests: XCTestCase {
         let users = try JSONDecoder().decode([User].self, from: data!)
         
         XCTAssertEqual(users.count, 2)
-        XCTAssertEqual(users[1].name, expectedName)
-        XCTAssertEqual(users[1].username, expectedUsername)
-        XCTAssertEqual(users[1].id, savedUser.id)
+        XCTAssertEqual(users[0].name, expectedName)
+        XCTAssertEqual(users[0].username, expectedUsername)
+        XCTAssertEqual(users[0].id, savedUser.id)
         
         conn.close()
     }
